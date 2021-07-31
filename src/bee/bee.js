a3a8120a79ab2ba0b3f8235a2d104f0fd5086ffd
AfterMetheFlood <AfterMetheFlood@outllok.com>
2021-07-31 12:11:52 -0500

    bee

---
+++ src/bee/bee.js
@@ -0,0 +1,51 @@
+function drawCircle(x, y, radius, fillCircle) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  if (fillCircle) {
+    ctx.fill();
+  } else {
+    ctx.stroke();
+  }
+}
+
+function drawBee(x, y) {
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = "Black";
+  ctx.fillStyle = "Gold";
+
+  // draw the bee's body with black
+  drawCircle(x, y, 8, false);
+  // Fill the cicle with gold color
+  drawCircle(x, y, 8, true);
+  // draw tow eyes
+  drawCircle(x - 2, y - 1, 2, false);
+  drawCircle(x + 2, y - 1, 2, false);
+  // draw two ears
+  drawCircle(x - 5, y - 11, 5, false);
+  drawCircle(x + 5, y - 11, 5, false);
+}
+
+function updateCoordinate(coordinate) {
+  console.log(Math.random());
+  var offset = Math.random() * 10 - 5;
+  coordinate += offset;
+  if (coordinate > 300) {
+    coordinate = 300;
+  }
+  if (coordinate < 0) {
+    coordinate = 0;
+  }
+  return coordinate;
+}
+
+var canvas = document.getElementById("beeCanvas");
+var ctx = canvas.getContext("2d");
+var x = 150;
+var y = 150;
+setInterval(function () {
+  ctx.clearRect(0, 0, 300, 300);
+  ctx.strokeRect(0, 0, 300, 300);
+  drawBee(x, y);
+  x = updateCoordinate(x);
+  y = updateCoordinate(y);
+}, 30);
